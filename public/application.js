b535d28a516bbc9fb4b147dd73ca7cb110986cc9
Scott Covert <scott.b.covert@gmail.com>
2015-08-12 00:20:07 -0400

    Updated sidenav icons and menu options

--- public/application.js
+++ public/application.js
@@ -16,13 +16,17 @@ angular.module(ApplicationConfiguration.applicationModuleName).config([
         // Register the user `avatar` icons
         $mdIconProvider
             .defaultIconSet('./assets/svg/avatars.svg', 128)
-            .icon('menu'       , './assets/svg/menu.svg'        , 24)
-            .icon('share'      , './assets/svg/share.svg'       , 24)
-            .icon('google_plus', './assets/svg/google_plus.svg' , 512)
-            .icon('hangouts'   , './assets/svg/hangouts.svg'    , 512)
-            .icon('twitter'    , './assets/svg/twitter.svg'     , 512)
-            .icon('phone'      , './assets/svg/phone.svg'       , 512)
-            .icon('flash'      , './assets/svg/flash.svg'       , 512);
+            .icon('menu'                    , './assets/svg/menu.svg'                   , 24)
+            .icon('share'                   , './assets/svg/share.svg'                  , 24)
+            .icon('google_plus'             , './assets/svg/google_plus.svg'            , 512)
+            .icon('hangouts'                , './assets/svg/hangouts.svg'               , 512)
+            .icon('twitter'                 , './assets/svg/twitter.svg'                , 512)
+            .icon('phone'                   , './assets/svg/phone.svg'                  , 512)
+            .icon('flash'                   , './assets/svg/flash.svg'                  , 512)
+            .icon('personal_folder'         , './assets/svg/personal_folder.svg'        , 512)
+            .icon('starred_folder'          , './assets/svg/starred_folder.svg'         , 512)
+            .icon('trending'                , './assets/svg/trending.svg'               , 512)
+            .icon('new'                     , './assets/svg/new.svg'                    , 512)
     }
 ]);
 
